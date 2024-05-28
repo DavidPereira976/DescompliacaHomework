2173d1e2bab4061c83f5b6cf0113671b50344284
DavidPereira976 <davidpereira2002rj@gmail.com>
2024-05-28 18:45:39 -0300

    Modificação de estilos e comportamento

---
+++ src/App2.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+
+function App2() {
+  return (
+    <div>
+      <Button variant="contained" color="primary">
+        Meu Botão Customizado
+      </Button>
+    </div>
+  );
+}
+
+export default App2;
